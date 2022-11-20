350c2b65f2153d2783dbd83531d5edc001ff63d0
Manthan Ankolekar <manthan.ank@gmail.com>
2022-11-21 00:42:41 +0530

    changes made

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,7 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { PostsService } from './posts/posts.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  MatButtonModule
+} from "@angular/material/button";
+import {
+  MatToolbarModule
+} from "@angular/material/toolbar";
+import {
+  MatExpansionModule
+} from "@angular/material/expansion";
+import {
+  MatCardModule,
+} from "@angular/material/card";
+import {
+  MatInputModule,
+} from "@angular/material/input";
 @NgModule({
   declarations: [
     AppComponent
@@ -12,9 +30,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    PostCreateComponent,
+    PostListComponent,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule
   ],
-  providers: [],
+  providers: [PostsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
