fcf89986c3b5d2243f1e2bc053685c4978d03491
Manthan Ankolekar <manthan.ank@gmail.com>
2022-11-21 00:31:35 +0530

    changes made

---
+++ frontend/src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-post-create',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent {
+
+}
---
+++ frontend/src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-post-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent {
+
+}
