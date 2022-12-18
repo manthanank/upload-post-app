f6765baab097dc9026031292215a05a93e7793db
Manthan Ankolekar <manthan.ank@gmail.com>
2022-12-18 21:03:09 +0530

    changes made

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,5 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-import { inject } from '@vercel/analytics';
-
-inject();
-
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
